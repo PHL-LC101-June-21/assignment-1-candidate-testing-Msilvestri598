c61fbab0683ee48e826138adafa9b07d0d3fd718
Mackenzie Silvestri <85536181+Msilvestri598@users.noreply.github.com>
2021-06-14 20:48:28 +0000

    I used readline-sync and if/else conditionals

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " "
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space?  "
+let correctAnswer = "Sally Ride"
+let candidateAnswer = " "
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,21 +15,32 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+const input = require('readline-sync');
 
+let candidateName = input.question("What is your name? ");
+console.log("Hello " + candidateName + ", welcome!")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  const input = require('readline-sync');
 
+  let candidateAnswer = input.question(question)
+  
 
+ if (candidateAnswer.toUpperCase() == correctAnswer.toUpperCase()) { 
+   console.log("Great job!")  
+ } else {
+   console.log("WRONG!")
+ }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
+ let grade;
+  
+  
   
 
   return grade;
